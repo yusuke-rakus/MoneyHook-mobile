35cccf46f33a4b97078b2876abcf458374ad2e99
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2022-11-29 22:33:08 +0900

    メールアドレス変更画面の作成

--- lib/src/modals/settings_modal/changeEmail.dart
+++ lib/src/modals/settings_modal/changeEmail.dart
@@ -10,42 +10,67 @@ class ChangeEmail extends StatefulWidget {
 class _ChangeEmailState extends State<ChangeEmail> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ListView(
-          padding: const EdgeInsets.all(8),
-          children: [
-            Container(
-              color: Colors.amber,
-              height: 50,
-              width: double.infinity,
-            )
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
-              height: 60,
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25))),
-                  fixedSize: const Size(double.infinity, 50),
+    return Scaffold(
+      appBar: AppBar(
+        title: (const Text('設定')),
+      ),
+      body: Stack(
+        children: [
+          ListView(
+            children: [
+              Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  height: 35,
+                  child: const Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Text(
+                        'メールアドレス変更',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ))),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'メールアドレス',
+                    icon: Icon(Icons.email_outlined),
+                  ),
                 ),
-                child: const Text(
-                  '登録',
-                  style: TextStyle(fontSize: 23, letterSpacing: 20),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'パスワード',
+                    icon: Icon(Icons.vpn_key_rounded),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: 60,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25))),
+                    fixedSize: const Size(double.infinity, 50),
+                  ),
+                  child: const Text(
+                    '登録',
+                    style: TextStyle(fontSize: 23, letterSpacing: 20),
+                  ),
                 ),
               ),
             ),
-          ),
-        )
-      ],
-    )
-      ;
+          )
+        ],
+      ),
+    );
   }
 }
