6b5a7a968ac817e94d79265867979af0ab5b6fd9
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2023-01-08 22:24:59 +0900

    収支入力画面の更新

--- lib/src/modals/editTransaction.dart
+++ lib/src/modals/editTransaction.dart
@@ -116,208 +116,221 @@ class _EditTransaction extends State<EditTransaction> {
             ),
           ],
         ),
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: [
-            // 日付
-            InkWell(
-              onTap: () {
-                showCupertinoModalPopup(
-                  context: context,
-                  builder: (_) => Container(
-                    height: 250,
-                    color: Colors.white,
-                    child: CupertinoDatePicker(
-                      initialDateTime: DateFormat('yyyy-MM-dd')
-                          .parse(transaction.transactionDate),
-                      onDateTimeChanged: (value) {
-                        setState(() {
-                          transaction.transactionDate =
-                              DateFormat('yyyy-MM-dd').format(value);
-                        });
-                      },
-                      minimumYear: DateTime.now().year - 1,
-                      maximumYear: DateTime.now().year,
-                      maximumDate: DateTime.now(),
-                      dateOrder: DatePickerDateOrder.ymd,
-                      mode: CupertinoDatePickerMode.date,
-                    ),
-                  ),
-                );
-              },
-              child: SizedBox(
-                height: 60,
-                child: Center(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      '${transaction.transactionDate.replaceAll('-', '月').replaceFirst('月', '年')}日',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    const Icon(Icons.edit),
-                  ],
-                )),
-              ),
-            ),
-            // 金額
-            Container(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              height: 100,
-              // color: Colors.cyanAccent,
-              child: Row(
-                children: [
-                  Switcher(
-                    value: transaction.transactionSign > 0 ? true : false,
-                    size: SwitcherSize.medium,
-                    enabledSwitcherButtonRotate: false,
-                    onChanged: (bool state) {
-                      state
-                          ? transaction.transactionSign = 1
-                          : transaction.transactionSign = -1;
-                    },
-                    iconOff: Icons.remove,
-                    colorOff: Colors.red,
-                    iconOn: Icons.add,
-                    colorOn: Colors.green,
-                  ),
-                  Container(
-                    width: 20,
-                  ),
-                  Flexible(
-                    child: TextField(
-                      onChanged: (value) {
-                        setState(() {
-                          if (value.isNotEmpty) {
-                            transaction.transactionAmount =
-                                transactionClass.formatInt(value);
-                          } else {
-                            transaction.transactionAmount = 0;
-                          }
-                        });
-                      },
-                      controller: amountController,
-                      decoration: InputDecoration(
-                          hintText: '¥0',
-                          hintStyle:
-                              const TextStyle(fontSize: 20, letterSpacing: 8),
-                          errorText:
-                              transaction.transactionAmountError.isNotEmpty
-                                  ? transaction.transactionAmountError
-                                  : null),
-                      keyboardType: TextInputType.number,
-                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // 取引名
-            Container(
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              height: 100,
-              alignment: Alignment.center,
-              child: TextField(
-                onChanged: (value) {
-                  setState(() {
-                    transaction.transactionName = value;
-                  });
-                },
-                controller: nameController,
-                decoration: InputDecoration(
-                    labelText: '取引名',
-                    errorText: transaction.transactionNameError.isNotEmpty
-                        ? transaction.transactionNameError
-                        : null),
-                style: const TextStyle(fontSize: 20),
-              ),
-            ),
-            // カテゴリ
-            Container(
-              margin: const EdgeInsetsDirectional.fromSTEB(40, 30, 40, 30),
-              child: InkWell(
-                onTap: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SelectCategory(env),
+        body: Stack(children: [
+          ListView(
+            padding: const EdgeInsets.all(8),
+            children: [
+              // 日付
+              InkWell(
+                onTap: () {
+                  showCupertinoModalPopup(
+                    context: context,
+                    builder: (_) => Container(
+                      height: 250,
+                      color: Colors.white,
+                      child: CupertinoDatePicker(
+                        initialDateTime: DateFormat('yyyy-MM-dd')
+                            .parse(transaction.transactionDate),
+                        onDateTimeChanged: (value) {
+                          setState(() {
+                            transaction.transactionDate =
+                                DateFormat('yyyy-MM-dd').format(value);
+                          });
+                        },
+                        minimumYear: DateTime.now().year - 1,
+                        maximumYear: DateTime.now().year,
+                        maximumDate: DateTime.now(),
+                        dateOrder: DatePickerDateOrder.ymd,
+                        mode: CupertinoDatePickerMode.date,
+                      ),
                     ),
                   );
-                  if (result == null) return;
-                  setState(() {
-                    transaction.categoryName = result['categoryName'];
-                    transaction.categoryId = result['categoryId'];
-                    transaction.subCategoryName = result['subCategoryName'];
-                    transaction.subCategoryId = result['subCategoryId'];
-                  });
                 },
                 child: SizedBox(
-                  height: 70,
-                  child: InputDecorator(
-                    decoration: const InputDecoration(
-                      labelText: 'カテゴリ',
+                  height: 60,
+                  child: Center(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${transaction.transactionDate.replaceAll('-', '月').replaceFirst('月', '年')}日',
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      const Icon(Icons.edit),
+                    ],
+                  )),
+                ),
+              ),
+              // 金額
+              Container(
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                height: 100,
+                // color: Colors.cyanAccent,
+                child: Row(
+                  children: [
+                    Switcher(
+                      value: transaction.transactionSign > 0 ? true : false,
+                      size: SwitcherSize.medium,
+                      enabledSwitcherButtonRotate: false,
+                      onChanged: (bool state) {
+                        state
+                            ? transaction.transactionSign = 1
+                            : transaction.transactionSign = -1;
+                      },
+                      iconOff: Icons.remove,
+                      colorOff: Colors.red,
+                      iconOn: Icons.add,
+                      colorOn: Colors.green,
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '${transaction.categoryName} / ${widget.transaction.subCategoryName}',
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(fontSize: 20),
+                    Container(
+                      width: 20,
+                    ),
+                    Flexible(
+                      child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            if (value.isNotEmpty) {
+                              transaction.transactionAmount =
+                                  transactionClass.formatInt(value);
+                            } else {
+                              transaction.transactionAmount = 0;
+                            }
+                          });
+                        },
+                        controller: amountController,
+                        decoration: InputDecoration(
+                            hintText: '¥0',
+                            hintStyle:
+                                const TextStyle(fontSize: 20, letterSpacing: 8),
+                            errorText:
+                                transaction.transactionAmountError.isNotEmpty
+                                    ? transaction.transactionAmountError
+                                    : null),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // 取引名
+              Container(
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                height: 100,
+                alignment: Alignment.center,
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      transaction.transactionName = value;
+                    });
+                  },
+                  controller: nameController,
+                  decoration: InputDecoration(
+                      labelText: '取引名',
+                      errorText: transaction.transactionNameError.isNotEmpty
+                          ? transaction.transactionNameError
+                          : null),
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+              // カテゴリ
+              Container(
+                margin: const EdgeInsetsDirectional.fromSTEB(40, 30, 40, 30),
+                child: InkWell(
+                  onTap: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SelectCategory(env),
+                      ),
+                    );
+                    if (result == null) return;
+                    setState(() {
+                      transaction.categoryName = result['categoryName'];
+                      transaction.categoryId = result['categoryId'];
+                      transaction.subCategoryName = result['subCategoryName'];
+                      transaction.subCategoryId = result['subCategoryId'];
+                    });
+                  },
+                  child: SizedBox(
+                    height: 70,
+                    child: InputDecorator(
+                      decoration: const InputDecoration(
+                        labelText: 'カテゴリ',
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              '${transaction.categoryName} / ${widget.transaction.subCategoryName}',
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 20),
+                            ),
                           ),
-                        ),
-                        const Align(
-                          alignment: Alignment.centerRight,
-                          child: Icon(
-                            Icons.keyboard_arrow_right,
-                            size: 30,
-                          ),
-                        )
-                      ],
+                          const Align(
+                            alignment: Alignment.centerRight,
+                            child: Icon(
+                              Icons.keyboard_arrow_right,
+                              size: 30,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            // 固定費フラグ
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.only(left: 40, right: 40),
-              height: 100,
-              child: CheckboxListTile(
-                activeColor: Colors.blue,
-                title: const Text('固定費として計算する'),
-                value: transaction.fixedFlg,
-                onChanged: (value) {
-                  setState(() {
-                    transaction.fixedFlg = !transaction.fixedFlg;
-                  });
-                },
+              // 固定費フラグ
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(left: 40, right: 40),
+                height: 100,
+                child: CheckboxListTile(
+                  activeColor: Colors.blue,
+                  title: const Text('固定費として計算する'),
+                  value: transaction.fixedFlg,
+                  onChanged: (value) {
+                    setState(() {
+                      transaction.fixedFlg = !transaction.fixedFlg;
+                    });
+                  },
+                ),
               ),
-            ),
-            // 登録
-            SizedBox(
-                height: 60,
-                child: ElevatedButton(
-                  onPressed: transaction.isDisabled()
-                      ? null
-                      : () {
-                          setState(() {
-                            _editTransaction(transaction, env);
-                          });
-                        },
-                  style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(25)))),
-                  child: const Text(
-                    '登録',
-                    style: TextStyle(fontSize: 23, letterSpacing: 20),
+            ],
+          ),
+          // 登録
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  color: Colors.white,
+                  height: 60,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: transaction.isDisabled()
+                        ? null
+                        : () {
+                            setState(() {
+                              _editTransaction(transaction, env);
+                            });
+                          },
+                    style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25)))),
+                    child: const Text(
+                      '登録',
+                      style: TextStyle(fontSize: 23, letterSpacing: 20),
+                    ),
                   ),
                 )),
-          ],
-        ));
+          )
+        ]));
   }
 }
