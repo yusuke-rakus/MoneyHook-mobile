7d0cb32684fd094eef9150d52fe5448825602579
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2023-01-09 10:06:02 +0900

    さっきの残り

--- lib/src/modals/settings_modal/changeEmail.dart
+++ lib/src/modals/settings_modal/changeEmail.dart
@@ -56,103 +56,111 @@ class _ChangeEmailState extends State<ChangeEmail> {
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = FocusNode();
     final passwordController = TextEditingController(text: emailClass.password);
     passwordController.selection = TextSelection.fromPosition(
         TextPosition(offset: passwordController.text.length));
 
-    return Scaffold(
-      appBar: AppBar(
-        title: (const Text('設定')),
-      ),
-      body: Stack(
-        children: [
-          ListView(
+    return Focus(
+      focusNode: focusNode,
+      child: GestureDetector(
+        onTap: focusNode.requestFocus,
+        child: Scaffold(
+          appBar: AppBar(
+            title: (const Text('設定')),
+          ),
+          body: Stack(
             children: [
-              Container(
-                  padding: const EdgeInsets.only(left: 10),
-                  height: 35,
-                  child: const Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Text(
-                        'メールアドレス変更',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ))),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  onChanged: (value) {
-                    setState(() {
-                      emailClass.email = value;
-                    });
-                  },
-                  controller: emailController,
-                  decoration: InputDecoration(
-                      labelText: '新しいメールアドレス',
-                      icon: const Icon(Icons.email_outlined),
-                      errorText: emailClass.emailError.isNotEmpty
-                          ? emailClass.emailError
-                          : null),
-                ),
+              ListView(
+                children: [
+                  Container(
+                      padding: const EdgeInsets.only(left: 10),
+                      height: 35,
+                      child: const Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text(
+                            'メールアドレス変更',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ))),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          emailClass.email = value;
+                        });
+                      },
+                      controller: emailController,
+                      decoration: InputDecoration(
+                          labelText: '新しいメールアドレス',
+                          icon: const Icon(Icons.email_outlined),
+                          errorText: emailClass.emailError.isNotEmpty
+                              ? emailClass.emailError
+                              : null),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      obscureText: !_showPassword,
+                      onChanged: (value) {
+                        setState(() {
+                          emailClass.password = value;
+                        });
+                      },
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          labelText: "パスワード",
+                          icon: const Icon(Icons.vpn_key_rounded),
+                          errorText: emailClass.passwordError.isNotEmpty
+                              ? emailClass.passwordError
+                              : null,
+                          suffixIcon: IconButton(
+                            icon: Icon(_showPassword
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: () {
+                              setState(() {
+                                _showPassword = !_showPassword;
+                              });
+                            },
+                          )),
+                    ),
+                  ),
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  obscureText: !_showPassword,
-                  onChanged: (value) {
-                    setState(() {
-                      emailClass.password = value;
-                    });
-                  },
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                      labelText: "パスワード",
-                      icon: const Icon(Icons.vpn_key_rounded),
-                      errorText: emailClass.passwordError.isNotEmpty
-                          ? emailClass.passwordError
-                          : null,
-                      suffixIcon: IconButton(
-                        icon: Icon(_showPassword
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined),
-                        onPressed: () {
-                          setState(() {
-                            _showPassword = !_showPassword;
-                          });
-                        },
-                      )),
+                padding: const EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    height: 60,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: emailClass.isDisabled()
+                          ? null
+                          : () {
+                              setState(() {
+                                _changeEmail();
+                              });
+                            },
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        fixedSize: const Size(double.infinity, 50),
+                      ),
+                      child: const Text(
+                        '登録',
+                        style: TextStyle(fontSize: 23, letterSpacing: 20),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+              )
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 60,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: emailClass.isDisabled()
-                      ? null
-                      : () {
-                          setState(() {
-                            _changeEmail();
-                          });
-                        },
-                  style: ElevatedButton.styleFrom(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25))),
-                    fixedSize: const Size(double.infinity, 50),
-                  ),
-                  child: const Text(
-                    '登録',
-                    style: TextStyle(fontSize: 23, letterSpacing: 20),
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
--- lib/src/modals/settings_modal/changePassword.dart
+++ lib/src/modals/settings_modal/changePassword.dart
@@ -52,6 +52,7 @@ class _ChangePasswordState extends State<ChangePassword> {
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = FocusNode();
     final passwordController =
         TextEditingController(text: passwordClass.password);
     passwordController.selection = TextSelection.fromPosition(
@@ -67,152 +68,159 @@ class _ChangePasswordState extends State<ChangePassword> {
     newPassword2Controller.selection = TextSelection.fromPosition(
         TextPosition(offset: newPassword2Controller.text.length));
 
-    return Scaffold(
-      appBar: AppBar(
-        title: (const Text('設定')),
-      ),
-      body: Stack(
-        children: [
-          ListView(
+    return Focus(
+      focusNode: focusNode,
+      child: GestureDetector(
+        onTap: focusNode.requestFocus,
+        child: Scaffold(
+          appBar: AppBar(
+            title: (const Text('設定')),
+          ),
+          body: Stack(
             children: [
-              Container(
-                  padding: const EdgeInsets.only(left: 10),
-                  height: 35,
-                  child: const Align(
-                      alignment: Alignment.bottomLeft,
-                      child: Text(
-                        'パスワード変更',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ))),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  obscureText: !_showCurrentPassword,
-                  onChanged: (value) {
-                    setState(() {
-                      passwordClass.password = value;
-                    });
-                  },
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                      labelText: "現在のパスワード",
-                      errorText: passwordClass.passwordError.isNotEmpty
-                          ? passwordClass.passwordError
-                          : null,
-                      suffixIcon: IconButton(
-                        icon: Icon(_showCurrentPassword
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined),
-                        onPressed: () {
-                          setState(() {
-                            _showCurrentPassword = !_showCurrentPassword;
-                          });
-                        },
-                      )),
-                ),
+              ListView(
+                children: [
+                  Container(
+                      padding: const EdgeInsets.only(left: 10),
+                      height: 35,
+                      child: const Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Text(
+                            'パスワード変更',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ))),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      obscureText: !_showCurrentPassword,
+                      onChanged: (value) {
+                        setState(() {
+                          passwordClass.password = value;
+                        });
+                      },
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          labelText: "現在のパスワード",
+                          errorText: passwordClass.passwordError.isNotEmpty
+                              ? passwordClass.passwordError
+                              : null,
+                          suffixIcon: IconButton(
+                            icon: Icon(_showCurrentPassword
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: () {
+                              setState(() {
+                                _showCurrentPassword = !_showCurrentPassword;
+                              });
+                            },
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      obscureText: !_showNewPassword,
+                      onChanged: (value) {
+                        setState(() {
+                          passwordClass.newPassword = value;
+                        });
+                      },
+                      controller: newPasswordController,
+                      decoration: InputDecoration(
+                          labelText: "新しいパスワード",
+                          errorText: passwordClass.newPasswordError.isNotEmpty
+                              ? passwordClass.newPasswordError
+                              : null,
+                          suffixIcon: IconButton(
+                            icon: Icon(_showNewPassword
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: () {
+                              setState(() {
+                                _showNewPassword = !_showNewPassword;
+                              });
+                            },
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      obscureText: !_showNewPassword2,
+                      onChanged: (value) {
+                        setState(() {
+                          passwordClass.newPassword2 = value;
+                        });
+                      },
+                      controller: newPassword2Controller,
+                      decoration: InputDecoration(
+                          labelText: "再入力",
+                          errorText: passwordClass.newPassword2Error.isNotEmpty
+                              ? passwordClass.newPassword2Error
+                              : null,
+                          suffixIcon: IconButton(
+                            icon: Icon(_showNewPassword2
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: () {
+                              setState(() {
+                                _showNewPassword2 = !_showNewPassword2;
+                              });
+                            },
+                          )),
+                    ),
+                  ),
+                ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  obscureText: !_showNewPassword,
-                  onChanged: (value) {
-                    setState(() {
-                      passwordClass.newPassword = value;
-                    });
-                  },
-                  controller: newPasswordController,
-                  decoration: InputDecoration(
-                      labelText: "新しいパスワード",
-                      errorText: passwordClass.newPasswordError.isNotEmpty
-                          ? passwordClass.newPasswordError
-                          : null,
-                      suffixIcon: IconButton(
-                        icon: Icon(_showNewPassword
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined),
-                        onPressed: () {
-                          setState(() {
-                            _showNewPassword = !_showNewPassword;
-                          });
-                        },
-                      )),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextField(
-                  obscureText: !_showNewPassword2,
-                  onChanged: (value) {
-                    setState(() {
-                      passwordClass.newPassword2 = value;
-                    });
-                  },
-                  controller: newPassword2Controller,
-                  decoration: InputDecoration(
-                      labelText: "再入力",
-                      errorText: passwordClass.newPassword2Error.isNotEmpty
-                          ? passwordClass.newPassword2Error
-                          : null,
-                      suffixIcon: IconButton(
-                        icon: Icon(_showNewPassword2
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined),
-                        onPressed: () {
-                          setState(() {
-                            _showNewPassword2 = !_showNewPassword2;
-                          });
-                        },
-                      )),
+                padding: const EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    height: 60,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: passwordClass.isDisabled()
+                          ? null
+                          : () {
+                              setState(() {
+                                _changePassword();
+                                // if (passwordClass.errorMessage.isNotEmpty) {
+                                //   showDialog(
+                                //       context: context,
+                                //       builder: (BuildContext context) =>
+                                //           AlertDialog(
+                                //             content:
+                                //                 Text(passwordClass.errorMessage),
+                                //             actions: [
+                                //               TextButton(
+                                //                 onPressed: () {
+                                //                   Navigator.pop(context);
+                                //                 },
+                                //                 child: const Text('閉じる'),
+                                //               )
+                                //             ],
+                                //           ));
+                                // }
+                              });
+                            },
+                      style: ElevatedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(25))),
+                        fixedSize: const Size(double.infinity, 50),
+                      ),
+                      child: const Text(
+                        '登録',
+                        style: TextStyle(fontSize: 23, letterSpacing: 20),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 60,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: passwordClass.isDisabled()
-                      ? null
-                      : () {
-                          setState(() {
-                            _changePassword();
-                            // if (passwordClass.errorMessage.isNotEmpty) {
-                            //   showDialog(
-                            //       context: context,
-                            //       builder: (BuildContext context) =>
-                            //           AlertDialog(
-                            //             content:
-                            //                 Text(passwordClass.errorMessage),
-                            //             actions: [
-                            //               TextButton(
-                            //                 onPressed: () {
-                            //                   Navigator.pop(context);
-                            //                 },
-                            //                 child: const Text('閉じる'),
-                            //               )
-                            //             ],
-                            //           ));
-                            // }
-                          });
-                        },
-                  style: ElevatedButton.styleFrom(
-                    shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25))),
-                    fixedSize: const Size(double.infinity, 50),
-                  ),
-                  child: const Text(
-                    '登録',
-                    style: TextStyle(fontSize: 23, letterSpacing: 20),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
