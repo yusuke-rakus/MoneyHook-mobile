a0c9b3bf1f874f196163897c3dabaf688e73d825
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2023-10-08 00:06:17 +0900

    flutterバージョンアップの変更

--- lib/src/env/googleSignIn.dart
+++ lib/src/env/googleSignIn.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:money_hooks/firebase_options.dart';
 
 Future<UserCredential> signInWithGoogle() async {
   // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  final GoogleSignInAccount? googleUser = await GoogleSignIn(
+          clientId: DefaultFirebaseOptions.currentPlatform.iosClientId)
+      .signIn();
 
   // Obtain the auth details from the request
   final GoogleSignInAuthentication? googleAuth =
