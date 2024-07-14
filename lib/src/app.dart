28d41b96b5d296689d27eaee54b1596d1b651e3f
YusukeMatsumoto <yusuke.matsumoto@rakus-partners.co.jp>
2024-07-14 10:10:03 +0900

    ログイン画面のアップデート

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
